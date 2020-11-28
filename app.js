f3c251e112e310897aacff8a19c9606b21d4879b
RoiWu <roih1h116@gmail.com>
2020-11-28 21:41:52 +0800

    add upload image function

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
 
+app.use('/upload', express.static(__dirname + '/upload'))
 app.engine('hbs', handlebars({ defaultLayout: 'main', extname: '.hbs' })) // Handlebars 註冊樣板引擎
 app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎
 
