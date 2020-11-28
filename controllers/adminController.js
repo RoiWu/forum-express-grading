07a030412c18b51081930128c7921723af308865
RoiWu <roih1h116@gmail.com>
2020-11-28 21:13:42 +0800

    add admit create restaurant page/get/post

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -6,8 +6,29 @@ const adminController = {
     return Restaurant.findAll({ raw: true }).then(restaurants => {
       return res.render('admin/restaurants', { restaurants: restaurants })
     })
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back') //record will be clear, modify later.
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
-
 
 module.exports = adminController
