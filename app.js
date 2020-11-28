4e7a9816fecd00a77b110bfe1569a6b2b67c00d4
RoiWu <roih1h116@gmail.com>
2020-11-28 22:15:01 +0800

    using imgur to save images

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const methodOverride = require('method-override')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('./models') // 引入資料庫
 const app = express()
 const port = 3000
