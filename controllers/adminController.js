480dead17942d99e2eb729e7a56b501da9a2cbc7
RoiWu <roih1h116@gmail.com>
2020-12-04 22:51:37 +0800

    done add categories in admin create restaurant

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -21,9 +21,15 @@ const adminController = {
     })
   },
 
-
   createRestaurant: (req, res) => {
-    return res.render('admin/create')
+    Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      return res.render('admin/create', {
+        categories: categories
+      })
+    })
   },
 
   postRestaurant: (req, res) => {
