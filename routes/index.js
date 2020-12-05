79dbd6dddef6b6f176949ab566b8865e74aabf75
RoiWu <roih1h116@gmail.com>
2020-12-05 17:08:09 +0800

    add user single restaurant comment get

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const restController = require('../controllers/restController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 const categoryController = require('../controllers/categoryController.js')
+const commentController = require('../controllers/commentController.js')
+
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 const helplers = require('../_helpers')
@@ -27,6 +29,7 @@ module.exports = (app, passport) => {
   app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
   app.get('/restaurants', authenticated, restController.getRestaurants)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
+  app.post('/comments', authenticated, commentController.postComment)
 
   //admin
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
