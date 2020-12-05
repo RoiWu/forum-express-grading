3ed6407ea5e9defa25f4a9c3b61b09182399ad22
RoiWu <roih1h116@gmail.com>
2020-12-05 17:24:56 +0800

    add admin delete comments function

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ module.exports = (app, passport) => {
   app.get('/restaurants', authenticated, restController.getRestaurants)
   app.get('/restaurants/:id', authenticated, restController.getRestaurant)
   app.post('/comments', authenticated, commentController.postComment)
+  app.delete('/comments/:id', authenticated, commentController.deleteComment)
 
   //admin
   app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
