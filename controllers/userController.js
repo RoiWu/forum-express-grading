94608e34fef97440aab332a2cf5a60626330a238
RoiWu <roih1h116@gmail.com>
2020-12-05 19:21:52 +0800

    add user profile put

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const User = db.User //input the user schema
 
+const imgur = require('imgur-node-api')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+
 const userController = {
   //////////////
   //sign in
@@ -80,6 +83,45 @@ const userController = {
         }
       })
   },
+
+  putUser: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+
+    const { file } = req
+    if (file) {
+      imgur.setClientID(IMGUR_CLIENT_ID);
+      imgur.upload(file.path, (err, img) => {
+        return User.findByPk(req.params.id)
+          .then((user) => {
+            user.update({
+              name: req.body.name,
+              image: file ? img.data.link : user.image
+            })
+              .then((user) => {
+                req.flash('success_messages', 'user was successfully to update')
+                res.redirect(`/users/${req.params.id}`)
+              })
+          })
+      })
+    }
+    else {
+      return User.findByPk(req.params.id)
+        .then((user) => {
+          user.update({
+            name: req.body.name,
+            image: user.image
+          })
+            .then((user) => {
+              req.flash('success_messages', 'user was successfully to update')
+              res.redirect(`/users/${req.params.id}`)
+            })
+        })
+    }
+  },
+
 }
 
 module.exports = userController
