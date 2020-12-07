4aa8846a1717d7f2c27914b5b444daae56a38b0b
RoiWu <roih1h116@gmail.com>
2020-12-07 21:06:30 +0800

    create /api/admin/categories

--- routes/apis.js
+++ routes/apis.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../controllers/api/adminController.js')
+const categoryController = require('../controllers/api/categoryController.js')
 
 const { authenticated, authenticatedAdmin, isOwnProfile, editOwnProfile } = require('../middleware/check-auth')
 
@@ -9,5 +10,6 @@ const passport = require('../config/passport')
 
 router.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+router.get('/admin/categories', authenticatedAdmin, categoryController.getCategories)
 
 module.exports = router
---
+++ services/categoryService.js
@@ -0,0 +1,26 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
+const Category = db.Category
+
+const categoryController = {
+  getCategories: (req, res, callback) => {
+    return Category.findAll({
+      raw: true,
+      nest: true
+    }).then(categories => {
+      if (req.params.id) {
+        Category.findByPk(req.params.id)
+          .then((category) => {
+            callback({
+              categories: categories,
+              category: category.toJSON()
+            })
+          })
+      } else {
+        callback({ categories: categories })
+      }
+    })
+  }
+}
+
+module.exports = categoryController
